e76ab36eb9369001b9448022f72ea54f07cb2908
sumeyyekyc <77787700+sumeyyekyc@users.noreply.github.com>
2021-02-23 18:01:38 +0100

    Update home.tpl.php

--- app/views/main/home.tpl.php
+++ app/views/main/home.tpl.php
@@ -1,1 +1,102 @@
-<h1>Home-page à faire</h1>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'accueil</title>
+    <!-- celui que j'ai crée -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body class="homepage">
+    <!-- First paragraphe with the image -->
+
+    <table>
+        <tr>
+            <td class="paragraph">
+                <h1>
+                    Mieux s'organiser dans son travail pour mieux réussir
+                </h1>
+                <p>
+                    <b>Doing Done</b> est l'outil numérique idéal qui vous permettra d'organiser votre travail grâce à la méthode Kanban qui a déjà fait ses preuves. Ayez le bon réflexe d'étiqueter vos tâches en gardant une vue d'ensemble sur vos projet afin de ne louper aucun processus de réalisation. 
+                </p>
+            </td>
+    
+            <td class="homeimg">
+                    <img class="image" src="_img/laptop.png" alt="screen">
+            </td>
+        </tr>
+    </table>
+    
+
+    <!-- Second paragraphe + image -->
+    <table>
+        <tr>
+            <td class="homeimg">
+                <img class="image" src="_img/heads.jpg" alt="heads">
+            </td>
+
+            <td class="paragraph">
+                <h1>
+                    À qui s'adresse Doind Done ? 
+                </h1>
+                <p>
+                    À toute personne désireuse d'adopter une méthodologie dans son organisation, que ce soit pour votre travail ou bien pour vos tâches du quotidien. 
+                </p>
+            </td>
+        </tr>
+    </table>
+
+
+    <!-- Third paragraph + image -->
+    <table>
+        <tr>
+            <td class="paragraph">
+                <h1>
+                 Notre objectif
+                </h1>
+                <p>
+                 Nous cherchons à rendre les travaux laborieux plsu simples grâce à des méthodologies managériales utilisées par les plus grandes entreprises. 
+                </p>
+            </td>
+
+            <td class="homeimg">
+                <img class="image" src="_img/mountain.jpg" alt="mountain">
+            </td>
+        </tr>
+    </table>
+
+    <!-- Fourth paragraph + image -->
+    <table>
+        <tr>
+            <td class="homeimg">
+                <img class="image" src="_img/workspace.jpg" alt="workspace">
+            </td>
+
+            <td class="paragraph">
+                <h1>
+                    Pourquoi utiliser Doing Done ? 
+                </h1>
+                <p>
+                    Nous avons élaboré <b>Doing Done</b> afin d'apporter une expérience simple car la réussite de la gestion d'un projet ne réside pas dans une interface complexe mais bien dans sa simplicité. 
+                    <br>
+                    Et la bonne nouvelle est que c'est totalement <b>gratuit</b> ! 
+                </p>
+            </td>
+        </tr>
+    </table>
+
+    <!-- Video demo -->
+
+    <div class="demo">
+        <h1>
+            Démonstration en vidéo
+        </h1>
+        <img class="image" src="_img/player3.png" alt="image_from_pixabay">
+    </div>
+     <!-- insérer la viédo ici -->
+
+
+
+</body>
+</html>
